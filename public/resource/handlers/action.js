a65d92de2f5b8ecc917b7b12b817736b31b24c60
Chris Wade <genghishack@gmail.com>
2021-02-26 01:52:12 -0700

    Removed public endpoints with no authentication

--- public/resource/handlers/action.js
+++
@@ -1,7 +0,0 @@
-import {buildResponse, success, failure} from '../../../lib/response-lib';
-
-const actionHandlers = {
-  GET: {},
-};
-
-export default actionHandlers;
--- public/resource/router.js
+++
@@ -1,7 +0,0 @@
-import {LambdaRouter} from "../../lib/lambda-lib";
-import ResourceHandlers from './handlers';
-
-export const main = LambdaRouter({
-  handlers: ResourceHandlers,
-  idType: 'uuid',
-});
